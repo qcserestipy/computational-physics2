f75dab2924ab71084da0d2892ffa2893fe1bc8ad
Patrick Eschenbach <p_esch01@wwu.de>
2019-01-14 12:38:26 +0100

    save

--- Uebung11/discFT_2.f95
+++ Uebung11/discFT_2.f95
@@ -50,15 +50,16 @@ program main
         omega(i) = i * ((2*PI)/spec_points*dt)
     enddo
 
-    write(*,*) "####################################"
-    write(*,*) "#        Entering power spec       #" 
-    write(*,*) "####################################"
+    write(*,*) "#########################################"
+    write(*,*) "#        Entering power spectrum        #" 
+    write(*,*) "#########################################"
     open(unit=1,file="power_spec.out")
     do i = 1, spec_points
         power_spec(i) = abs(f_k(i))**2
         write(1,*) omega(i), power_spec(i)
     enddo
     close(1)
+
 
 end program main
 
@@ -111,28 +112,3 @@ subroutine dft(f_x, f_k, N)
     close(1)
 
 end subroutine dft
-
-
-! Super simple routine that executes discrete fourier back transform
-! f_x : x-space data points (double precision complex array with size N)
-! f_k : k-space data points (double precision complex array with size N)
-! N   : the size of f_x and f_k
-subroutine bdft(f_x, f_k, N)
-    implicit none
-    integer, intent(in) :: N
-    complex*16, dimension(N), intent(out) :: f_x
-    complex*16, dimension(N), intent(in) :: f_k
-    real*16, parameter :: PI = 4 * ATAN(1.)
-    integer :: i,j
-
-    do i=1,N
-      ! initializing f_k(i)
-      f_x(i) = 0
-      
-      ! main calculation part
-      do j=1,N
-          f_x(i) = f_k(i) + f_k(j) * cexp(cmplx(0, 2*PI*i*j/N))
-      end do
-    end do
-
-end subroutine bdft
